26ec69445c6ed7713dbfabb3f73458b705885c10
Roman <306863030@qq.com>
2026-07-22 15:50:15 +0800

    更新

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'deepfish-ai-server',
     script: './dist/serve/pm2-server.js',
-    cwd: '../',
+    cwd: './',
     node_args: '--no-warnings',
     env: {
       NODE_ENV: 'production',
